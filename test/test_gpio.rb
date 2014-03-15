ea0d07a886eb1ac208c9f17fcfe13548cb84b93c
Michal Pawlowski <misza222@gmail.com>
2014-03-15 11:13:37 +0100

    Add empty tests for Gpio class

---
+++ test/test_gpio.rb
@@ -0,0 +1,25 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'intel_galileo'
+
+describe IntelGalileo::Gpio do
+  describe "initialization" do
+    it "exports gpio"
+  end
+  
+  describe "when reading" do
+    it "ensures direction"
+  end
+  
+  describe "when writing" do
+    it "ensures direction"
+  end
+  
+  describe "ensure direction" do
+    it "sets direction if not set"
+  end
+  
+  describe "ensure drive" do
+    it "sets drive if not set"
+  end
+end
